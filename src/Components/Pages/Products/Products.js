cbed744d3baf5d484fed53f07545bd58eeca9407
Vibolrottana Seng <rattana90@yahoo.com>
2021-09-03 21:56:29 +0700

    Update Products.js

--- src/Components/Pages/Products/Products.js
+++ src/Components/Pages/Products/Products.js
@@ -1,16 +1,18 @@
-import React,{useContext} from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../contexts/AuthContext';
 import HeroSection from '../../HeroSection';
 import { homeObjOne, homeObjTwo, } from './Data';
 
 
 function Products() {
-    
-   
 
-   
+
+
+
+
     return (
         <>
-            
+
             <HeroSection {...homeObjOne}></HeroSection>
             <HeroSection {...homeObjTwo} />
         </>
